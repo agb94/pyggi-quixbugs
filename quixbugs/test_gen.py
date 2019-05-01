547bc7130bca09e3023cb4282c50f52fb2f050d1
Gabin An <agb94@kaist.ac.kr>
2019-05-01 23:38:53 +0900

    add preconditions

--- quixbugs/test_gen.py
+++ quixbugs/test_gen.py
@@ -15,42 +15,96 @@ def handler(signum, frame):
 
 TARGETS = [
     'next_permutation',
+    'possible_change',
     'lis',
     'mergesort',
     'bucketsort',
     'shunting_yard',
-    'hanoi',
     'gcd',
     'to_base',
-    'possible_change',
+    'hanoi',
     'bitcount',
     'wrap',
 ]
 
-TRY = 1000
+TRY = 200
 
-def gen(correct, buggy, base):
+def is_valid_input(module_name, tinput):
+    try:
+        if module_name == 'next_permutation':
+            #A list of unique ints
+            perm = tinput[0]
+            return isinstance(perm, list) and all([isinstance(i, int) for i in perm]) and len(list(set(perm))) == len(perm) and not (list(reversed(sorted(perm))) == perm)
+        elif module_name == 'lis':
+            arr = tinput[0]
+            # The ints in arr are unique
+            return isinstance(arr, list) and all([isinstance(i, int) for i in arr]) and len(list(set(arr))) == len(arr)
+        elif module_name == 'mergesort':
+            arr = tinput[0]
+            return isinstance(arr, list) and all([isinstance(i, int) for i in arr])
+        elif module_name == 'bucketsort':
+            arr, k = tuple(tinput)
+            return all(isinstance(x, int) and 0 <= x < k for x in arr)
+        elif module_name == 'shunting_yard':
+            tokens = tinput[0]
+            return all(isinstance(token, int) or token in '+-*/' for token in tokens)
+        elif module_name == 'hanoi':
+            height, start, end = tuple(tinput)
+            return height >= 0 and start in (1, 2, 3) and end in (1, 2, 3)
+        elif module_name == 'gcd':
+            a, b = tuple(tinput)
+            return isinstance(a, int) and isinstance(b, int) and a >= 0 and b >= 0
+        elif module_name == 'to_base':
+            num, b = tuple(tinput)
+            return num > 0 and 2 <= b <= 36
+        elif module_name == 'possible_change':
+            #coins: A list of positive ints representing coin denominations
+            #total: An int value to make change for
+            coins, total = tuple(tinput)
+            return isinstance(coins, list) and all([isinstance(x, int) and x > 0 for x in coins])
+        elif module_name == 'bitcount':
+            n = tinput[0]
+            return n >= 0
+        elif module_name == 'wrap':
+            #text: The starting text.
+            #cols: The target column width, i.e. the maximum length of any single line after wrapping.
+            text, cols = tuple(tinput)
+            return cols > 0
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
+    except:
+        return False
+
+def gen(correct, buggy, tests, module_name):
     signal.signal(signal.SIGALRM, handler)
     #print(correct(*base))
     #print(buggy(*base))
-    passing = []
-    for i in range(TRY):
-        neighbours = get_neighbours(base, 2, 0.001)
-        for neighbour in neighbours:
-            try:
-                signal.alarm(TIMELIMIT)
-                correct_result = correct(*neighbour)
-                signal.alarm(TIMELIMIT)
-                buggy_result = buggy(*neighbour)
-                if correct_result == buggy_result:
-                    print([neighbour, correct_result])
-                    passing.append(neighbour)
-                    return neighbour
-            except KeyboardInterrupt:
-                raise KeyboardInterrupt
-            except:
-                pass
-        base = random.choice(neighbours)
+    passing = list()
+    for test in tests:
+        base = test[0]
+        if not isinstance(base, list):
+            base = [base]
+        for i in range(TRY):
+            #if i % 10 == 0:
+                #print(base, i)
+            neighbours = get_neighbours(base, 2, 0.001)
+            for neighbour in neighbours:
+                try:
+                    signal.alarm(TIMELIMIT)
+                    correct_result = correct(*neighbour)
+                    signal.alarm(TIMELIMIT)
+                    buggy_result = buggy(*neighbour)
+                    if correct_result == buggy_result:
+                        if neighbour not in passing and is_valid_input(module_name, neighbour):
+                            #print([neighbour, correct_result])
+                            passing.append([neighbour, correct_result])
+                        #return neighbour
+                except KeyboardInterrupt:
+                    raise KeyboardInterrupt
+                except Exception as e:
+                    pass
+            base = random.choice(neighbours)
+    return passing
 
 def run(module_name):
     correct_module = importlib.import_module("correct_python_programs.{}".format(module_name))
@@ -62,11 +116,12 @@ def run(module_name):
         for l in f.readlines():
             if l.strip():
                 tests.append(eval(l.replace('true', 'True').replace('false', 'False')))
-    base = tests[0][0]
-    if not isinstance(base, list):
-        base = [base]
     print(module_name)
-    result = gen(correct_func, buggy_func, base)
+    result = gen(correct_func, buggy_func, tests, module_name)
+    print(len(result))
+    with open("passing_{}.json".format(module_name), 'a') as f:
+        for item in result:
+            f.write(str(item) + '\n')
     #if result:
     #    print(module_name, result)
 
