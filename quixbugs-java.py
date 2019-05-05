5a409bf1200ad36eda5c05ee4a02ad715f4193b6
Gabin An <agb94@kaist.ac.kr>
2019-05-05 14:05:46 +0900

    xml setup

--- quixbugs-java.py
+++ quixbugs-java.py
@@ -12,9 +12,10 @@ from pyggi.algorithms import LocalSearch
 
 QUIXBUGS_DIR = os.path.abspath("./quixbugs")
 JAVA_DIR = os.path.join(QUIXBUGS_DIR, "java_programs")
+JAVA_XML_DIR = os.path.join(QUIXBUGS_DIR, "java_xml_programs")
 JSON_DIR = os.path.join(QUIXBUGS_DIR, "json_testcases")
 TARGETS = list(map(lambda s: s.strip(), open('TARGETS', 'r').readlines()))
-COMPILE_COMMAND = "javac LIS.java -d ."
+COMPILE_COMMAND = "javac {}.java -d ."
 TEST_COMMAND = "java -cp gson-2.8.2.jar:. JavaDeserialization {} {}"
 
 class MyLineProgram(LineProgram):
@@ -46,7 +47,32 @@ class MyLineProgram(LineProgram):
         return result
 
 class MyTreeProgram(TreeProgram):
-    pass
+    def run(self, timeout=10):
+        try_compile = self.exec_cmd(COMPILE_COMMAND.format(self.algo.upper()), timeout)
+        if try_compile.stderr:
+            result = self.__class__.Result(
+                            status_code=StatusCode.PARSE_ERROR,
+                            elapsed_time=try_compile.elapsed_time,
+                            stdout=try_compile.stdout,
+                            stderr=try_compile.stderr)
+        else:
+            failing = 0
+            elapsed_time = 0
+            for test in self.tests:
+                test_in, test_out = test
+                algo_input = ['"'+json.dumps(arg)+'"' for arg in copy.deepcopy(test_in)]
+                try_test = self.exec_cmd(TEST_COMMAND.format(self.algo, ' '.join(algo_input)), timeout)
+                #print(TEST_COMMAND.format(self.algo, ' '.join(algo_input)))
+                #print(try_test.stdout.strip(), str(test_out))
+                if try_test.stdout is None or try_test.stdout.rstrip() != str(test_out):
+                    failing += 1
+                elapsed_time += try_test.elapsed_time
+            result = self.__class__.Result(
+                status_code=StatusCode.NORMAL,
+                elapsed_time=elapsed_time,
+                stdout=str(failing),
+                stderr='')
+        return result
 
 def run(mode, target, epoch, maxiter):
     target_path = os.path.join(JAVA_DIR, target + '.py')
@@ -57,15 +83,20 @@ def run(mode, target, epoch, maxiter):
     print(test_path)
     print("----------------------------------------------")
 
-    config = {
-        'target_files': [target.upper() + '.java'],
-        'test_command': None
-    }
+
     if mode == 'line':
+        config = {
+            'target_files': [target.upper() + '.java'],
+            'test_command': None
+        }
         program = MyLineProgram(JAVA_DIR, config=config)
         operators = [LineReplacement, LineInsertion, LineDeletion]
     elif mode == 'tree':
-        program = MyTreeProgram(JAVA_DIR, config=config)
+        config = {
+            'target_files': [target.upper() + '.java.xml'],
+            'test_command': None
+        }
+        program = MyTreeProgram(JAVA_XML_DIR, config=config)
         operators = [StmtReplacement, StmtInsertion, StmtDeletion]
     program.algo = target
     program.tests = []
@@ -77,8 +108,8 @@ def run(mode, target, epoch, maxiter):
                 test_in = [test_in]
             program.tests.append((test_in, test_out))
 
-    print(program.run())
     patch = Patch(program)
+    #print(program.run())
     print(program.evaluate_patch(patch))
 
     class MyLocalSearch(LocalSearch):
@@ -108,14 +139,11 @@ def run(mode, target, epoch, maxiter):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    #parser.add_argument('--mode', type=str, default='line')
+    parser.add_argument('--mode', type=str, default='line')
     #parser.add_argument('--target', type=str, default='gcd')
-    parser.add_argument('--epoch', type=int, default=30)
-    parser.add_argument('--maxiter', type=int, default=100)
+    parser.add_argument('--epoch', type=int, default=20)
+    parser.add_argument('--maxiter', type=int, default=500)
     args = parser.parse_args()
-    run('line', 'lis', args.epoch, args.maxiter)
-    """
-    for mode in ['line', 'tree']:
-        for target in TARGETS:
-            run(mode, target, args.epoch, args.maxiter)
-    """
+
+    for target in TARGETS:
+        run(args.mode, target, args.epoch, args.maxiter)
